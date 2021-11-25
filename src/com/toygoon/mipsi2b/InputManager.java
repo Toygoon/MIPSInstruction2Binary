fd1b0e44e6bf29b54e9f798840b9fbe28606e106
Toygoon <toygoon@gmail.com>
2021-11-25 22:28:34 +0900

    misc

--- src/com/toygoon/mipsi2b/InputManager.java
+++ src/com/toygoon/mipsi2b/InputManager.java
@@ -15,13 +15,16 @@ public class InputManager {
 
     public static ArrayList<String> inputText() {
         Scanner s = new Scanner(System.in);
+
         System.out.print("> ");
         // User input from here.
         String input = s.nextLine();
-        String[] splitInput = input.split(" ");
 
+        // Splits the String with space to the String array.
+        String[] splitInput = input.split(" ");
         ArrayList<String> separated = new ArrayList<>();
         for(int i=0; i<splitInput.length; i++) {
+            // Comma should be deleted.
             splitInput[i] = splitInput[i].replaceAll(",", "");
             separated.add(splitInput[i]);
         }
