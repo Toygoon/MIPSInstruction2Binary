9c3e1a0434ddb683c041cf30afdfd6093d9c7c88
Toygoon <toygoon@gmail.com>
2021-11-25 22:32:23 +0900

    Saving prompt

--- src/com/toygoon/mipsi2b/InputManager.java
+++ src/com/toygoon/mipsi2b/InputManager.java
@@ -4,6 +4,19 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class InputManager {
+    public static boolean isSaving() {
+        Scanner s = new Scanner(System.in);
+        System.out.println(">> Do you want to save to the file? (y or n)");
+        System.out.print("> ");
+        String input = s.nextLine();
+        input.toLowerCase();
+
+        if(input.contains("y"))
+            return true;
+
+        return false;
+    }
+
     public static ArrayList<String> inputText() {
         Scanner s = new Scanner(System.in);
 
@@ -14,11 +27,13 @@ public class InputManager {
         // Splits the String with space to the String array.
         String[] splitInput = input.split(" ");
         ArrayList<String> separated = new ArrayList<>();
+
         for(int i=0; i<splitInput.length; i++) {
             // Comma should be deleted.
             splitInput[i] = splitInput[i].replaceAll(",", "");
             separated.add(splitInput[i]);
         }
+
         return separated;
     }
 }
